4458945f7d448ebdadf5bfcf72e5c7b766a177a3
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-01-23 11:34:07 +0900

    コード整形

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import useSWR from 'swr'
+import useSWR from "swr";
 import "./App.css";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
-  const fetcher = (url) => fetch(url, {headers}).then(res => res.json())
+  const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR(url, fetcher)
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
-  if (error) return <p className='status'>Failed to load.</p>
-  if (isLoading) return <p className='status'>Loading...</p>
+  if (error) return <p className="status">Failed to load.</p>;
+  if (isLoading) return <p className="status">Loading...</p>;
 
-  return <>{data && <p className='status'>Status : OK </p>}</>;
-
+  return <>{data && <p className="status">Status : OK </p>}</>;
 }
 
 export default App;
