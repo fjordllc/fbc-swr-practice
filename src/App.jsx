29a70aac866d00bb2dc89bf73bfc221f5c268575
zecky1120 <takahide1120@gmail.com>
2025-09-17 20:27:51 +0900

    fix: 初期状態へ戻す

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import useSWR from "swr";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const url = "http://localhost:3001/200?sleep=2000";
 const headers = { Accept: "application/json" };
-const fetcher = async (url) => {
-  const res = await fetch(url, { headers });
-  if (!res.ok) {
-    throw new Error(`Error, status: ${res.status}`);
-  }
-  return res.json();
-};
 
 function App() {
-  const { data, error, isLoading } = useSWR(url, fetcher);
-  if (error) return <p>Failed to load.</p>;
-  if (isLoading) return <p>Loading...</p>;
-  return <>{data && <p>Status: {data.description}</p>}</>;
+  const [status, setStatus] = useState("");
+  
+  useEffect(() => {
+    fetch(url, { headers })
+      .then((res) => res.json())
+      .then((json) => setStatus(json.description))
+  }, []);
+
+  return <>{status && <p>Status : {status}</p>}</>;
 }
 
 export default App;
